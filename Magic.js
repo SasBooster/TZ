11a0f7935fc048d68e3a192827cd81ae689409f8
Artemiy <xyolox1337@gmail.com>
2023-12-12 15:20:38 +0300

    modal window

---
+++ Magic.js
@@ -0,0 +1,7 @@
+document.getElementById("Bt_openModal").addEventListener("click", function() {
+  document.getElementById("modal1").classList.add("open")
+})
+
+document.getElementById("close-modal1").addEventListener("click", function() {
+  document.getElementById("modal1").classList.remove("open")
+})
